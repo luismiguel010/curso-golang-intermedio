4f2b8a9c7015885aa356fbccd086d3dab181082f
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-08-31 14:16:09 -0500

    code first lesson

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	//Declaración de vbles
 	var x int
 	x = 8
 	y := 7
 	fmt.Println(x)
 	fmt.Println(y)
+
+	//Controlando error
+	myValue, err := strconv.ParseInt("NaN", 0, 64)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}else {
+		fmt.Println(myValue)
+	}
+	//maps
+	m := make(map[string]int)
+	m["key"] = 6
+	fmt.Println(m["key"])
+
+	//Slice de enteros
+	s:= []int{1,2,3}
+	for index, value := range s {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+	s = append(s, 16)
+	for index, value := range s {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+
 }
