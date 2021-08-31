c20c17c3ae1b5ff4409746650f267683d203623f
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-08-31 15:58:06 -0500

    Se añade clase para crear constructor

--- poo/structs/class.go
+++ poo/structs/class.go
@@ -7,9 +7,26 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
-	e.id = 1
-	e.name = "Name"
-	fmt.Printf("%v", e)
+	e.SetId(5)
+	e.SetName("Name2")
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
