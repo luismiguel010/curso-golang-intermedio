d50c1c05d8e99e6224e98b5f8d7d93b2953029c6
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-08-31 17:52:51 -0500

    Se añade primera parte del código

---
+++ factory/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// SMS Email
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notification via SMS")
+}
+
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+
+}
+
+func (SMSNotificationSender) GetSenderMethod () string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel () string {
+	return "Twilio"
+}
