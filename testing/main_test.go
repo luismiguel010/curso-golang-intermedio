faa95fab1f6a2a01f1160d984497c51ed1343ede
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-09-01 14:28:05 -0500

    Se le añade test a la función GetMax

--- testing/main_test.go
+++ testing/main_test.go
@@ -25,3 +25,21 @@ func TestSum(t *testing.T) {
 		}
 	}
 }
+
+func TestMax(t *testing.T) {
+	tables:= []struct{
+		a int
+		b int
+		n int
+	} {
+		{4, 2, 4},
+		{3, 2, 3},
+	}
+
+	for _, item := range tables {
+		max := GetMax(item.a, item.b)
+		if max != item.n {
+			t.Errorf("GetMax was incorrect, got %d, expected %d", max, item.n)
+		}
+	}
+}
